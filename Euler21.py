62c263394afecc019f213aed61ef0de52b176cf4
George <fixerx4e00b4@gmail.com>
2018-07-12 04:58:53 +0300

    --

---
+++ Euler21.py
@@ -0,0 +1,29 @@
+def main():
+    ans = 0
+    r = list(range(6, 10001))
+    for i in r:
+        d1 = proper_divisor_sum(i)
+        d2 = proper_divisor_sum(d1)
+        if (d2 == i) and (d1 != i):
+            ans = ans + d1 + i
+            if d1 > i:
+                r.remove(d1)
+
+    print(ans)
+
+
+
+
+def proper_divisor_sum(n):
+    sum = 1
+    bar = n
+    current = 2
+    while current < bar:
+        if n % current == 0:
+            sum = sum + current + n / current
+            bar = n / current
+        current = current + 1
+    return sum
+
+if __name__ == '__main__':
+    main()
