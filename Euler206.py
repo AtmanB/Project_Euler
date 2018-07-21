c9ac1496f1d18ba0ca2aa7f952035a02656c07e8
George <fixerx4e00b4@gmail.com>
2018-07-21 18:38:34 +0300

    --

---
+++ Euler206.py
@@ -0,0 +1,19 @@
+#  3003 secs, needs revision
+
+import fnmatch
+
+from Timer import timethis
+
+
+@timethis
+def main():
+    TARGET = '1?2?3?4?5?6?7?8?9?0'
+    ans = 0
+    for n in range(1000000000, 1400000000):
+        if fnmatch.fnmatch(str(n ** 2), TARGET):
+            print(n)
+            break
+
+
+if __name__ == '__main__':
+    main()
