3c5f4ee2da8c7f204e780370efd0f7963881e28e
katia <noob-hawk-reborn@outlook.com>
2018-09-09 03:19:18 +0300

    --

--- project_euler.py
+++ project_euler.py
@@ -463,3 +463,35 @@ def choose(n, k):
     if n < k:
         return 0
     return math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
+
+def generate_continued_fraction_of_e(n):
+    seq = [1, 2]
+    idx = 2
+    jumper = 0
+    while idx < n:
+        idx += 1
+        if jumper == 2:
+            seq.append(seq[idx - 4] + 2)
+            jumper = 0
+        else:
+            seq.append(1)
+            jumper += 1
+    return seq
+
+
+def get_convergent_fraction(s, sequence):
+    n = 1
+    d = sequence[s - 2]
+    paidx = s - 3
+    pa = sequence[paidx]
+    steps = 1
+    while steps <= s - 2:
+        new_n = pa * d
+        n = new_n + n
+        temp = d
+        d = n
+        n = temp
+        paidx -= 1
+        pa = sequence[paidx]
+        steps += 1
+    return n + (2 * d), d
